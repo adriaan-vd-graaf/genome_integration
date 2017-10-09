0f72fe7dbd99968006d4a3e31605ecb2de1cad32
adriaan <adriaan.vd.graaf@gmail.com>
2017-10-09 10:56:23 +0200

    incremented lowest version in setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.0.dev0',
+    version='0.0.1.dev0',
     python_requires='!=3.0.*, !=3.1.*, !=3.2.*,!=3.3.*, !=3.4.*, >=3.5.*, <4',
 
 
