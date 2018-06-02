f23508b4d46ca685f720e1d72b7e6e699555dc3b
adriaan <adriaan.vd.graaf@gmail.com>
2018-06-02 18:16:09 +0200

    made a bit more sense I hope

---
+++ tests/resource_load_test.py
@@ -0,0 +1,6 @@
+from genome_integration.resources import read_gene_information
+
+def test_read_gene_info():
+    ensembl_info = read_gene_information()
+    ensembl_info.get_sorted_genes()
+    assert ensembl_info.ensg_to_gene["ENSG00000115607"] == "IL18RAP"
