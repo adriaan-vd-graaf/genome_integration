049e8cef89b325c04cf1da90acbace84d7d376e0
adriaan <adriaan.vd.graaf@gmail.com>
2019-04-23 16:14:51 +0200

    copyright notice removed

--- genome_integration/causal_inference/__init__.py
+++ genome_integration/causal_inference/__init__.py
@@ -4,7 +4,6 @@ inverse variance estimate on independent effects.
 """
 
 __author__      = "Adriaan van der Graaf"
-__copyright__   = "Copyright 2017, Adriaan van der Graaf"
 
 from genome_integration.causal_inference.mendelian_randomization import *
 from genome_integration.causal_inference.mr_link import *
