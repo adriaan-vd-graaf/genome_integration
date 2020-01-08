0656b387c53d8daf0dc987b7558fc6986be720ec
adriaan <adriaan.vd.graaf@gmail.com>
2020-01-08 15:08:03 +0100

    Removed GCTA cojo test, as it is not used in our analyses

--- tests/validate_plink_gcta_reimplementations.py
+++ tests/validate_plink_gcta_reimplementations.py
@@ -68,101 +68,6 @@ def turn_assocs_into_genetic_associations(assocs, ordered_loci, allele_frequency
     return assocs
 
 
-def test_compare_gcta_cojo_with_reference_no_missing():
-
-    np.random.seed(1313)
-
-    rel_path = '/'.join(('test_resources', 'subset_of_exposure_cohort'))
-
-    if len(__file__.split("/")) >1:
-        plink_loc = "{}/{}".format("/".join(__file__.split("/")[:-1]), rel_path)
-    else:
-        plink_loc = rel_path
-
-
-    temp_data = '/'.join(('temp_data', 'gcta_cojo_test'))
-
-    if len(__file__.split("/")) > 1:
-        temp_data = "{}/{}".format("/".join(__file__.split("/")[:-1]), temp_data)
-
-    plinkfile = utils.PlinkFile(plink_loc)
-    geno_mat = plinkfile.read_bed_file_into_numpy_array()
-    print(geno_mat.shape)
-    #one causal SNP.
-    # one causal SNP.
-    beta = [2, 2, -4]
-    phenotypes = simulate_mr.scale_geno_vec(geno_mat[:, 25]) * beta[0]
-    phenotypes += simulate_mr.scale_geno_vec(geno_mat[:, 7]) * beta[1]
-    phenotypes += simulate_mr.scale_geno_vec(geno_mat[:, 100]) * beta[2]
-    phenotypes += np.random.normal(size=phenotypes.shape)
-
-    phenotypes -= np.mean(phenotypes)
-    phenotypes /= np.std(phenotypes)
-
-    assocs = np.apply_along_axis(simulate_mr.do_gwas_on_scaled_variants, axis=0, arr=geno_mat, dependent=phenotypes).T
-
-
-    minor_allele_frequency = np.apply_along_axis(simulate_mr.geno_frq ,axis=0, arr=geno_mat).T
-    ordered_loci = np.asarray([plinkfile.bim_data.bim_results[x] for x in plinkfile.bim_data.snp_names])
-
-
-
-    sample_sizes = np.sum(geno_mat != 3, axis=0)
-    genetic_assocs = turn_assocs_into_genetic_associations(assocs,
-                                                           ordered_loci,
-                                                           minor_allele_frequency,
-                                                           sample_sizes)
-
-
-
-    gcta_assocs = utils.do_gcta_cojo_joint_on_genetic_associations(genetic_assocs,
-                                                        plink_loc,
-                                                        temp_data,
-                                                        p_val_thresh=0.05,
-                                                        maf= 0.0, _keep_ma_files=True)
-
-    #update the associations with the correct precision, GCTA also uses.
-    with open(temp_data + "_temp.ma", "r") as f:
-        f.readline()
-        for line in f:
-            split = line.split()
-            snp_name = split[0]
-            frq = float(split[3])
-            beta = float(split[4])
-            se = float(split[5])
-            genetic_assocs[snp_name].beta, \
-                genetic_assocs[snp_name].se, \
-                genetic_assocs[snp_name].minor_allele_frequency = (beta, se, frq)
-
-    subprocess.run(["rm", temp_data + "_temp.ma"], check=True)
-
-    selected_variants = sorted(gcta_assocs.ma_results.keys())
-
-    gcta_effects = np.asarray([[gcta_assocs.ma_results[x].beta, gcta_assocs.ma_results[x].se]
-                               for x in selected_variants])
-
-    indices = np.asarray([plinkfile.bim_data.snp_names.index(x) for x in selected_variants], dtype=int)
-
-    assocs_of_interest = {genetic_assocs[x].explanatory_name: genetic_assocs[x]
-                    for x, i in zip(plinkfile.bim_data.snp_names, range(len(ordered_loci))) if i in indices}
-
-
-    gcta_results_own = simulate_mr.do_gcta_cojo_conditional(
-        np.apply_along_axis(simulate_mr.scale_geno_vec, 0, geno_mat[:,indices]),
-        assocs_of_interest,
-        np.arange(3),
-        np.asarray(plinkfile.bim_data.snp_names)[indices]
-        )
-
-    # Effects deviate slightly due to floating point errors and inverting.
-    # last digits deviate slightly.
-    print(np.concatenate((gcta_results_own, gcta_effects), axis=1))
-    assert(np.all(np.isclose(gcta_results_own, gcta_effects, rtol=1e-4, atol=1e-3)))
-
-
-
-
-
 def test_compare_plink_assoc():
     np.random.seed(13289)
 
@@ -240,4 +145,4 @@ if not os.path.isdir(test_data_dir):
     os.mkdir(test_data_dir)
 
 test_compare_plink_assoc()
-test_compare_gcta_cojo_with_reference_no_missing()
+
