7d68efd9f1271cb223ff8120c5635aa5418875e9
adriaan <adriaan.vd.graaf@gmail.com>
2021-10-20 10:20:13 +0200

    Fixed the install issue.

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
-    install_requires=['numpy', 'scipy', 'sklearn', 'statsmodels', 'plinkio', 'requests', 'bitarray', 'SNPknock'],
+    install_requires=['numpy', 'scipy', 'sklearn', 'statsmodels', 'plinkio', 'requests', 'bitarray'],
 
     extras_require={
         'dev': ['check-manifest'],
